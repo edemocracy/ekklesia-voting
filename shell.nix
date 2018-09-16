df542e8c063ef00ef4d38c4ff56464e4dee807a2
Holger Burbach <holger@burbach.eu>
2018-09-16 12:52:16 +0200

    nix and pipenv added

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let
+  pkgs = import ./requirements/nixpkgs.nix;
+  pipenv = pkgs.pipenv;
+in with pkgs; stdenv.mkDerivation {
+  src = null;
+  name = "ekklesia_voting-dev-env";
+  phases = [];
+  buildInputs = with python37Packages; [ sassc pyflakes pipenv zsh postgresql100 python ];
+  #shellHook = "PYTHONPATH= SHELL=`which zsh` pipenv shell --fancy";
+}
